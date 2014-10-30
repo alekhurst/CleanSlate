be5aa8f5270892834111f2aa0cdd824e8e35a8ff
J.P. Ertola <ertolajp@gmail.com>
2014-10-29 21:38:18 -0700

    finished all_courses.js file

--- js/all_courses.js
+++ js/all_courses.js
@@ -1,42 +1,124 @@
 window.AllCourses = {
 	math_courses = [
 		{
-			name : "Math 11",
+			name : "Calculus & Analytic Geometry 1",
 			department : "MATH",
 			course_number : "11",
-			description : "Calculus I",
+			description : "something",
 			offering : ["F", "W", "S"],
 			co_requisite : "",
 			credit : "NO",
 			category : "math_and_science"
 		},
+
 		{
-			name : "Math 12",
+			name : "Calculus & Analytic Geometry 2",
 			department : "MATH",
 			course_number : "12",
-			description : "Calculus II",
+			description : "something",
 			offering : ["F", "W", "S"],
 			co_requisite : "",
 			credit : "NO",
 			category : "math_and_science"
-		}
+		},
+
 		{
-			name : "Math 13",
+			name : "Calculus & Analytic Geometry 3",
 			department : "MATH",
 			course_number : "13",
-			description : "Calculus III",
+			description : "something",
+			offering : ["F", "W", "S"],
+			co_requisite : "",
+			credit : "NO",
+			category : "math_and_science"
+		},
+
+		{
+			name : "Calculus & Analytic Geometry 4",
+			department : "MATH",
+			course_number : "14",
+			description : "something",
+			offering : ["F", "W", "S"],
+			co_requisite : "",
+			credit : "NO",
+			category : "math_and_science"
+		},
+
+		{
+			name : "Linear Algebra",
+			department : "MATH",
+			course_number : "53",
+			description : "something",
+			offering : ["F", "W", "S"],
+			co_requisite : "",
+			credit : "NO",
+			category : "math_and_science"
+		},
+
+		{
+			name : "Differential Equations",
+			department : "AMTH",
+			course_number : "106",
+			description : "something",
+			offering : ["F", "W", "S"],
+			co_requisite : "",
+			credit : "NO",
+			category : "math_and_science"
+		},
+
+		{
+			name : "Probability and Statistics",
+			department : "AMTH",
+			course_number : "108",
+			description : "something",
 			offering : ["F", "W", "S"],
 			co_requisite : "",
 			credit : "NO",
 			category : "math_and_science"
 		}
+	],
+
+	science_courses = [
 		{
-			name : "Math 14",
-			department : "MATH",
-			course_number : "14",
-			description : "Calculus IV",
-			offering : ["F", "W", "S"],
-			co_requisite : "",
+			name : "General Chemistry 1",
+			department : "CHEM",
+			course_number : "11",
+			description : "something",
+			offering : ["F"],
+			co_requisite : "LAB",
+			credit : "NO",
+			category : "math_and_science"
+		},
+
+		{
+			name : "Physics: Scientists & Engineers 1",
+			department : "PHYS",
+			course_number : "31",
+			description : "something",
+			offering : ["W"],
+			co_requisite : "LAB",
+			credit : "NO",
+			category : "math_and_science"
+		},
+
+		{
+			name : "Physics: Scientists & Engineers 2",
+			department : "PHYS",
+			course_number : "32",
+			description : "something",
+			offering : ["S"],
+			co_requisite : "LAB",
+			credit : "NO",
+			category : "math_and_science"
+		},
+
+		{
+			name : "Physics: Scientists & Engineers 3",
+			department : "PHYS",
+			course_number : "33",
+			description : "something",
+			offering : ["F"],
+			co_requisite : "LAB",
 			credit : "NO",
 			category : "math_and_science"
 		}
@@ -44,27 +126,89 @@ window.AllCourses = {
 
 	coen_courses = [
 		{
-			/*
-			To Do:
-			finish possible coen classes
-			*/
 			name : "Introduction to Programming",
 			department : "COEN",
 			course_number : "10",
+			description : "something",
+			offering : ["F", "W"],
+			co_requisite : "LAB",
+			credit : "NO",
+			category : "engineering"
+		},
+
+		{
+			name : "Advanced Programming in ",
+			department : "COEN",
+			course_number : "11",
 			description : "something",
 			offering : ["F", "W", "S"],
 			co_requisite : "LAB",
 			credit : "NO",
 			category : "engineering"
 		},
+
+		{
+			name : "Abstract Data Types and Data Structures",
+			department : "COEN",
+			course_number : "12",
+			description : "something",
+			offering : ["F", "W", "S"],
+			co_requisite : "LAB",
+			credit : "NO",
+			category : "engineering"
+		},
+
+		{
+			name : "Discrete Math",
+			department : "COEN",
+			course_number : "19",
+			description : "something",
+			offering : ["F", "S"],
+			co_requisite : "NO",
+			credit : "NO",
+			category : "engineering"
+		},
+
+		{
+			name : "Introduction to Embedded Systems",
+			department : "COEN",
+			course_number : "20",
+			description : "something",
+			offering : ["F", "S"],
+			co_requisite : "NO",
+			credit : "NO",
+			category : "engineering"
+		},
+
+		{
+			name : "Introduction to Logic Design",
+			department : "COEN",
+			course_number : "21",
+			description : "something",
+			offering : ["F", "W", "S"],
+			co_requisite : "LAB",
+			credit : "NO",
+			category : "engineering"
+		},
+
+		{
+			name : "Formal Specification and Advanced Data Structures",
+			department : "COEN",
+			course_number : "70",
+			description : "something",
+			offering : ["W"]
+			co_requisite : "LAB",
+			credit : "NO",
+			category : "engineering"
+		}
 	],
 
 
 	core_courses = [
 		{
-			name : "CTW 1",
-			department : "",
-			course_number : "",
+			name : "Critical Thinking & Writing 1",
+			department : "CTW",
+			course_number : "1",
 			description : "Critical Thinking & Writing I",
 			offering : ["F", "W"],
 			co_requisite : "",
@@ -73,9 +217,9 @@ window.AllCourses = {
 		},
 
 		{
-			name : "CTW 2",
-			department : "",
-			course_number : "",
+			name : "Critical Thinking & Writing 2",
+			department : "CTW",
+			course_number : "2",
 			description : "something",
 			offering : ["W", "S"],
 			co_requisite : "",
@@ -84,9 +228,9 @@ window.AllCourses = {
 		},	
 
 		{
-			name : "C&I 1",
-			department : "",
-			course_number : "",
+			name : "Cultures & Ideas 1",
+			department : "C&I",
+			course_number : "1",
 			description : "something",
 			offering : ["F", "W"],
 			co_requisite : "",
@@ -95,9 +239,9 @@ window.AllCourses = {
 		},
 
 		{
-			name : "CTW 2",
-			department : "",
-			course_number : "",
+			name : "Cultures & Ideas 2",
+			department : "C&I",
+			course_number : "2",
 			description : "something",
 			offering : ["W", "S"],
 			co_requisite : "",
@@ -105,9 +249,8 @@ window.AllCourses = {
 			category : "hummanities_and_social_science"
 		},
 
-
 		{
-			name : "Core",
+			name : "University Core",
 			department : "",
 			course_number : "",
 			description : "something",
