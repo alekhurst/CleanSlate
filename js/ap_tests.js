f4c679217744e6b32233a06b0c76b19c1d7d496c
Jordan Buschman <jordan.buschman@me.com>
2014-11-02 21:33:18 -0800

    Added tests to ap_tests.js

--- js/ap_tests.js
+++ js/ap_tests.js
@@ -1,1 +1,39 @@
-//This is where the library of AP tests go, plus what courses they test out of and what score you need to test out of them
+window.APTests = {
+    "Calculus AB" : {
+        min_score : "3",
+        max_score : "5",
+        fulfillment : [["math-courses", "MATH", "11"], ["math-courses", "MATH", "12"]],
+    },
+    "Calculus BC" : [
+        {
+            min_score : "3",
+            max_score : "4",
+            fulfillment : [["math-courses", "MATH", "11"]],
+        },
+        {
+            min_score : "4",
+            max_score : "5",
+            fulfillment : [["math-courses", "MATH", "11"], ["math-courses", "MATH", "12"], ["math-courses", "MATH", "13"]],
+        },
+    ],
+    "Chemistry" : {
+        min_score : "3",
+        max_score : "5",
+        fulfillment : [["science-courses", "CHEM", "11"]],
+    },
+    "Computer Science A" : {
+        min_score : "4",
+        max_score : "5",
+        fulfillment : [["coen-courses", "COEN", "11"]],
+    },
+    "Physics C: Mechanics" : {
+        min_score : "4",
+        max_score : "5",
+        fulfillment : [["science-courses", "PHYS", "31"]],
+    },
+    "Physics C: Electricity & Magnetism" : {
+        min_score : "4",
+        max_score : "5",
+        fulfillment : [["science-courses", "PHYS", "33"]],
+    },
+};
