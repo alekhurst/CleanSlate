608b25ddc9e7536758623b30aad267976e16d441
Jordan Buschman <jordan.buschman@me.com>
2014-11-03 23:41:10 -0800

    Corrected fullfillments in transfer_credit.js

--- js/transfer_credit.js
+++ js/transfer_credit.js
@@ -3,7 +3,7 @@ window.TransferCredit = [
         id : 0,
         name : "Calculus 1" ,
         description : 'something',
-        fulfillment : ["math-courses", "MATH", "11"]
+        fulfillment : [["math_courses", "MATH", "9"], ["math_courses", "MATH", "11"]]
     },
     {
         id : 1,
@@ -13,51 +13,51 @@ window.TransferCredit = [
         //     // {
         //     //     min_score : "3",
         //     //     max_score : "4",
-        //     //     fulfillment : [["math-courses", "MATH", "11"]],
+        //     //     fulfillment : [["math_courses", "MATH", "11"]],
         //     // },          
         // ],
-        fulfillment : ["math-courses", "MATH", "12"]
+        fulfillment : ["math_courses", "MATH", "12"]
     },
     {
         id : 2,
         name : "Chemistry 1",
         description : 'something',
-        fulfillment : ["science-courses", "CHEM", "11"]
+        fulfillment : ["science_courses", "CHEM", "11"]
     },
     {
         id : 3,
         name : "Computer Science 1",
         description : 'something',
-        fulfillment : [["coen-courses", "COEN", "10"]]
+        fulfillment : [["coen_courses", "COEN", "10"]]
     },
     {
         id : 4,
         name : "Computer Science 2",
         description : 'something',
-        fulfillment : [["coen-courses", "COEN", "11"]]
+        fulfillment : [["coen_courses", "COEN", "11"]]
     },
     {   
         id : 5,
         name : "Physics: Mechanics",
         description : 'something',
-        fulfillment : [["science-courses", "PHYS", "31"]]
+        fulfillment : [["science_courses", "PHYS", "31"]]
     },
     {
         id : 7,
         name : "Physics: Electricity & Magnetism",
         description : 'something',
-        fulfillment : [["science-courses", "PHYS", "33"]]
+        fulfillment : [["science_courses", "PHYS", "33"]]
     },
     {   
         id : 8,
         name : "English Composition 1",
         description : 'something',
-        fulfillment : [["humanities_and_social_science", "CTW", "1"]]
+        fulfillment : [["required_courses", "CTW", "1"]]
     },
     {   
         id : 9,
         name : "English Composition 2",
         description : 'something',
-        fulfillment : [["humanities_and_social_science", "CTW", "2"]]
+        fulfillment : [["required_courses", "CTW", "2"]]
     }
 ];
