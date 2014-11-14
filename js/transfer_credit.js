36b90e05d56a621ac3d450c5a3500aa552dd9d46
scott-m-sarsfield <scott.m.sarsfield@gmail.com>
2014-11-13 20:17:47 -0800

    Updated Transfer Credit (Why Have Sep. File?)

--- js/transfer_credit.js
+++ js/transfer_credit.js
@@ -1,63 +1,101 @@
 window.TransferCredit = [
     {
         id : 0,
-        name : "Calculus 1" ,
-        description : 'First calculus class',
-        fulfillment : [["math_courses", "MATH", "9"], ["math_courses", "MATH", "11"]]
+        name : "AMTH 106: Differential Equations",
+        description : 'Differential Equations',
+        fulfillment : [["math_courses", "AMTH", "106"]]
     },
     {
         id : 1,
-        name : "Calculus 2",
-        description : 'Second calculus class',
-        // [
-        //     // {
-        //     //     min_score : "3",
-        //     //     max_score : "4",
-        //     //     fulfillment : [["math_courses", "MATH", "11"]],
-        //     // },          
-        // ],
-        fulfillment : [["math_courses", "MATH", "9"], ["math_courses", "MATH", "11"], ["math_courses", "MATH", "12"]]
+        name : "AMTH 108: Probability and Statistics",
+        description : 'Probability and Statistics',
+        fulfillment : [["math_courses", "AMTH", "108"]]
     },
     {
         id : 2,
-        name : "Chemistry 1",
+        name : "CHEM 11: Chemistry I",
         description : 'first chemistry class',
         fulfillment : [["science_courses", "CHEM", "11"]]
     },
     {
         id : 3,
-        name : "Computer Science 1",
+        name : "COEN 10: Introduction to Programming",
         description : 'first computer science class',
         fulfillment : [["coen_courses", "COEN", "10"]]
     },
     {
         id : 4,
-        name : "Computer Science 2",
+        name : "COEN 11: Advanced Programming",
         description : 'second computer science class',
         fulfillment : [["coen_courses", "COEN", "10"], ["coen_courses", "COEN", "11"]]
     },
-    {   
+    {
         id : 5,
-        name : "Physics: Mechanics",
-        description : 'first physics class',
-        fulfillment : [["science_courses", "PHYS", "31"]]
+        name : "COEN 12: Abstract Data Types & Structures",
+        description : 'Abstract Data Types & Structures',
+        fulfillment : [["coen_courses", "COEN", "10"], ["coen_courses", "COEN", "11"],["coen_courses", "COEN", "12"]]
     },
     {
         id : 6,
-        name : "Physics: Electricity & Magnetism",
-        description : 'second physics class',
+        name : "COEN 19: Discrete Mathematics",
+        description : 'Discrete Mathematics',
+        fulfillment : [["coen_courses", "COEN", "19"]]
+    },
+	{
+        id : 7,
+        name : "MATH 11: Calculus I" ,
+        description : 'First calculus class',
+        fulfillment : [["math_courses", "MATH", "9"], ["math_courses", "MATH", "11"]]
+    },
+    {
+        id : 8,
+        name : "MATH 12: Calculus II",
+        description : 'Second calculus class',
+        fulfillment : [
+						["math_courses", "MATH", "9"],
+						["math_courses", "MATH", "11"],
+						["math_courses", "MATH", "12"]
+					  ]
+    },
+    {
+        id : 9,
+        name : "MATH 13: Calculus III",
+        description : 'Second calculus class',
+        fulfillment : [
+						["math_courses", "MATH", "9"], 
+						["math_courses", "MATH", "11"], 
+						["math_courses", "MATH", "12"], 
+						["math_courses", "MATH", "13"]
+					  ]
+    },
+    {
+        id : 10,
+        name : "MATH 14: Calculus IV",
+        description : 'Second calculus class',
+        fulfillment : [
+						["math_courses", "MATH", "9"], 
+						["math_courses", "MATH", "11"], 
+						["math_courses", "MATH", "12"], 
+						["math_courses", "MATH", "13"], 
+						["math_courses", "MATH", "14"]
+					  ]
+    },
+    {   
+        id : 11,
+        name : "PHYS 31: Physics for Engineers I",
+        description : 'Mechanics',
+        fulfillment : [["science_courses", "PHYS", "31"]]
+    },
+	{
+        id : 12,
+        name : "PHYS 32: Physics for Engineers II",
+        description : 'Gravitation, Fluids, Waves, Thermodynamics, Optics.',
+        fulfillment : [["science_courses", "PHYS", "32"]]
+	},
+    {
+        id : 13,
+        name : "PHYS 33: Physics for Engineers III",
+        description : 'Electricity and Magnetism',
         fulfillment : [["science_courses", "PHYS", "33"]]
     },
-    {   
-        id : 7,
-        name : "English Composition 1",
-        description : 'first english class',
-        fulfillment : [["CTW_courses", "CTW", "1"]]
-    },
-    {   
-        id : 8,
-        name : "English Composition 2",
-        description : 'second english class',
-        fulfillment : [["CTW_courses", "CTW", "2"]]
-    }
 ];
